ada9003cb4029e648220cea14504f1dc0150ab19
Jan van Mansum <jan.van.mansum@dans.knaw.nl>
2019-05-12 09:00:33 +0200

    Using the better solution without char entities from jibx-run 1.2.4.7-DANS

--- src/test/java/nl/knaw/dans/pf/language/ddm/api/Ddm2EmdCrosswalkTest.java
+++ src/test/java/nl/knaw/dans/pf/language/ddm/api/Ddm2EmdCrosswalkTest.java
@@ -98,6 +98,7 @@ public class Ddm2EmdCrosswalkTest {
             { "license" },
             { "normalRelation" },
             { "SMP.char" },
+            { "SMP.char.many" },
             { "spatialBox" },
             { "spatialGmlEnvelope" },
             { "spatialGmlPoints" },
