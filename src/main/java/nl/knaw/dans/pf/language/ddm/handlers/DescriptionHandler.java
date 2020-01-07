472c50128e1a6c63f0c8fb507a147c565321673f
Richard van Heest <richard.van.heest@dans.knaw.nl>
2020-01-07 10:58:06 +0100

    don't take the old prefix into account

--- src/main/java/nl/knaw/dans/pf/language/ddm/handlers/DescriptionHandler.java
+++ src/main/java/nl/knaw/dans/pf/language/ddm/handlers/DescriptionHandler.java
@@ -26,16 +26,11 @@ public class DescriptionHandler extends BasicStringHandler {
     final BasicString basicString = createBasicString(uri, localName);
 
     if (basicString != null) {
-      String oldPrefix = "Instructions for Reuse: ";
       String prefix = "Suggestions for data usage: ";
       String value = basicString.getValue();
       String desciptionType = getAttribute("", "descriptionType");
       boolean isTechnicalDescription = desciptionType != null && desciptionType.equals("TechnicalInfo");
-      boolean hasOldPrefix = value.toLowerCase().startsWith(oldPrefix.toLowerCase());
       boolean hasPrefix = value.toLowerCase().startsWith(prefix.toLowerCase());
-
-      if (isTechnicalDescription && hasOldPrefix)
-        value = value.replaceFirst(oldPrefix, "");
 
       if (isTechnicalDescription && !hasPrefix)
         basicString.setValue(prefix + value);
