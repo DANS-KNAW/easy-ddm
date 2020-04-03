91153ef5ea07b65c7e6c2159b9bfc2e547bd313d
lindar <linda.reijnhoudt@dans.knaw.nl>
2020-04-03 13:13:50 +0200

    Add Funding handler; Add tests; Upgrade easy-emd in pom

--- src/test/java/nl/knaw/dans/pf/language/ddm/api/Ddm2EmdTest.java
+++ src/test/java/nl/knaw/dans/pf/language/ddm/api/Ddm2EmdTest.java
@@ -91,6 +91,7 @@ public class Ddm2EmdTest {
             return true;
         }
         catch (IOException e) {
+            System.out.println(e.getMessage());
             return false;
         }
     }
@@ -162,6 +163,35 @@ public class Ddm2EmdTest {
                 if (xsdUri2Url.containsKey(uri))
                     assertThat(" namspace location " + file, url, is(xsdUri2Url.get(uri)));
             }
+        }
+    }
+
+    @Test
+    public void fundingTest() throws Exception {
+        externalSchemaCheck();
+        String emdXmlExpected = "src/test/resources/input/emd-actual-expected.xml";
+        String ddmXml = "src/test/resources/input/funding.xml";
+        String emdXmlActual = "target/emd-actual.xml";
+        InputSource inputSourceEmdXmlExpected = new InputSource(emdXmlExpected);
+        XPath xpath = createXpathInstance();
+        // Conversion
+        EasyMetadata emd = crosswalk.createFrom(new File(ddmXml));
+        assertNotNull(emd);
+        String emdString = new EmdMarshaller(emd).getXmlString();
+        // Write the conversion result
+        Files.write(Paths.get(emdXmlActual), emdString.getBytes(StandardCharsets.UTF_8));
+        // Compare only the <emd:creator> element
+        InputSource inputSourceEmdXmlActual = new InputSource(emdXmlActual);
+        XPathExpression expr = xpath.compile(".//emd:contributor");
+        Object emdCreatorExpectedObject = expr.evaluate(inputSourceEmdXmlExpected, XPathConstants.NODESET);
+        NodeList emdCreatorExptectedNodeList = (NodeList) emdCreatorExpectedObject;
+        Object emdCreatorActualObject = expr.evaluate(inputSourceEmdXmlActual, XPathConstants.NODESET);
+        NodeList emdCreatorActualNodeList = (NodeList) emdCreatorActualObject;
+
+        for (int i = 0; i < emdCreatorExptectedNodeList.getLength(); i++) {
+            Node emdCreatorExptectedNode = emdCreatorExptectedNodeList.item(i);
+            Node emdCreatorActualNode = emdCreatorActualNodeList.item(i);
+            compareNodes(emdCreatorExptectedNode, emdCreatorActualNode);
         }
     }
 
---
+++ src/test/java/nl/knaw/dans/pf/language/ddm/api/FundingTest.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2014 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.pf.language.ddm.api;
+import nl.knaw.dans.pf.language.emd.EasyMetadata;
+import nl.knaw.dans.pf.language.emd.binding.EmdMarshaller;
+import nl.knaw.dans.pf.language.xml.crosswalk.CrosswalkException;
+import nl.knaw.dans.pf.language.xml.exc.XMLSerializationException;
+import org.dom4j.tree.DefaultElement;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class FundingTest {
+
+
+  @Test
+  public void fundingMapTest() throws Exception {
+    // @formatter:off
+        String ddm = "<?xml version='1.0' encoding='utf-8'?><ddm:DDM" +
+                "  xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'" +
+                "  xmlns:ddm='http://easy.dans.knaw.nl/schemas/md/ddm/'" +
+                "  xmlns:dc='http://purl.org/dc/elements/1.1/'" +
+                "  xmlns:abr='http://www.den.nl/standaard/166/Archeologisch-Basisregister/'" +
+                "  xmlns:dcterms=\"http://purl.org/dc/terms/\"" +
+                ">" +
+                " <ddm:dcmiMetadata>" +
+                "  <ddm:funding>"
+                  + "<ddm:funderName>NWO</ddm:funderName>"
+                  + "<ddm:funderIdentifier funderIdentifierType='ISNI'>123</ddm:funderIdentifier>"
+                + "</ddm:funding>" +
+                " </ddm:dcmiMetadata>" +
+                "</ddm:DDM>";
+        // @formatter:on
+
+    /*
+    <emd:contributor>
+      <eas:contributor>
+        <eas:organization>NWO</eas:organization>
+        <eas:organizationId scheme="ISNI">123</eas:organizationId>
+        <eas:role scheme="EASY">Funder</eas:role>
+      <eas:contributor>
+    </emd:contributor>
+     */
+
+    DefaultElement top = firstEmdElementFrom(ddm);
+    DefaultElement sub = (DefaultElement) top.elements().get(0);
+    DefaultElement subsub1 = (DefaultElement) sub.elements().get(0);
+    DefaultElement subsub2 = (DefaultElement) sub.elements().get(1);
+    DefaultElement subsub3 = (DefaultElement) sub.elements().get(2);
+
+    assertThat(top.elements().size(), is(1));
+    assertThat(top.getQualifiedName(), is("emd:contributor"));
+    assertThat(sub.elements().size(), is(3));
+    assertThat(sub.getQualifiedName(), is("eas:contributor"));
+    assertEquals("eas:organization", subsub1.getQualifiedName());
+    assertEquals(1, subsub2.attributeCount());
+    assertEquals("ISNI", subsub2.attribute("scheme").getValue());
+    assertEquals("NWO", subsub1.getText());
+    assertEquals("123", subsub2.getText());
+    assertEquals("Funder", subsub3.getText());
+    assertEquals("EASY", subsub3.attribute("scheme").getValue());
+  }
+
+
+  private DefaultElement firstEmdElementFrom(String ddm) throws XMLSerializationException, CrosswalkException {
+    EasyMetadata emd = new Ddm2EmdCrosswalk(null).createFrom(ddm);
+    return (DefaultElement) new EmdMarshaller(emd).getXmlElement().elementIterator().next();
+  }
+}
